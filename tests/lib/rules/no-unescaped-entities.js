e6ad446b2ec37ea55bb1a7c58ffe7ed724a47b26
Patrick Hayes <pfhayes@gmail.com>
2016-07-14 21:17:59 -0700

    Remove babel-eslint

--- tests/lib/rules/no-unescaped-entities.js
+++ tests/lib/rules/no-unescaped-entities.js
@@ -10,6 +10,11 @@
 
 var rule = require('../../../lib/rules/no-unescaped-entities');
 var RuleTester = require('eslint').RuleTester;
+var parserOptions = {
+  ecmaFeatures: {
+    jsx: true
+  }
+};
 
 // ------------------------------------------------------------------------------
 // Tests
@@ -21,98 +26,98 @@ ruleTester.run('no-unescaped-entities', rule, {
   valid: [
     {
       code: [
-        'class Comp1 extends Component {',
-        '  render() {',
+        'var Hello = React.createClass({',
+        '  render: function() {',
         '    return (',
         '      <div/>',
         '    );',
         '  }',
-        '}'
+        '});'
       ].join('\n'),
-      parser: 'babel-eslint'
+      parserOptions: parserOptions
     }, {
       code: [
-        'class Comp1 extends Component {',
-        '  render() {',
+        'var Hello = React.createClass({',
+        '  render: function() {',
         '    return <div>Here is some text!</div>;',
         '  }',
-        '}'
+        '});'
       ].join('\n'),
-      parser: 'babel-eslint'
+      parserOptions: parserOptions
     }, {
       code: [
-        'class Comp1 extends Component {',
-        '  render() {',
+        'var Hello = React.createClass({',
+        '  render: function() {',
         '    return <div>I&rsquo;ve escaped some entities: &gt; &lt; &amp;</div>;',
         '  }',
-        '}'
+        '});'
       ].join('\n'),
-      parser: 'babel-eslint'
+      parserOptions: parserOptions
     }, {
       code: [
-        'class Comp1 extends Component {',
-        '  render() {',
+        'var Hello = React.createClass({',
+        '  render: function() {',
         '    return <div>first line is ok',
         '    so is second',
         '    and here are some escaped entities: &gt; &lt; &amp;</div>;',
         '  }',
-        '}'
+        '});'
       ].join('\n'),
-      parser: 'babel-eslint'
+      parserOptions: parserOptions
     }, {
       code: [
         'var Hello = React.createClass({',
-        '  render() {',
+        '  render: function() {',
         '    return <div>{">" + "<" + "&" + \'"\'}</div>;',
         '  },',
         '});'
       ].join('\n'),
-      parser: 'babel-eslint'
+      parserOptions: parserOptions
     }
   ],
 
   invalid: [
     {
       code: [
-        'class Comp1 extends Component {',
-        '  render() {',
+        'var Hello = React.createClass({',
+        '  render: function() {',
         '    return <div>></div>;',
         '  }',
-        '}'
+        '});'
       ].join('\n'),
-      parser: 'babel-eslint',
+      parserOptions: parserOptions,
       errors: [{message: 'HTML entities must be escaped.'}]
     }, {
       code: [
-        'class Comp1 extends Component {',
-        '  render() {',
+        'var Hello = React.createClass({',
+        '  render: function() {',
         '    return <div>first line is ok',
         '    so is second',
         '    and here are some bad entities: ></div>',
         '  }',
-        '}'
+        '});'
       ].join('\n'),
-      parser: 'babel-eslint',
+      parserOptions: parserOptions,
       errors: [{message: 'HTML entities must be escaped.'}]
     }, {
       code: [
-        'class Comp1 extends Component {',
-        '  render() {',
+        'var Hello = React.createClass({',
+        '  render: function() {',
         '    return <div>\'</div>;',
         '  }',
-        '}'
+        '});'
       ].join('\n'),
-      parser: 'babel-eslint',
+      parserOptions: parserOptions,
       errors: [{message: 'HTML entities must be escaped.'}]
     }, {
       code: [
-        'class Comp1 extends Component {',
-        '  render() {',
+        'var Hello = React.createClass({',
+        '  render: function() {',
         '    return <div>Multiple errors: \'>></div>;',
         '  }',
-        '}'
+        '});'
       ].join('\n'),
-      parser: 'babel-eslint',
+      parserOptions: parserOptions,
       errors: [
         {message: 'HTML entities must be escaped.'},
         {message: 'HTML entities must be escaped.'},
@@ -120,13 +125,13 @@ ruleTester.run('no-unescaped-entities', rule, {
       ]
     }, {
       code: [
-        'class Comp1 extends Component {',
-        '  render() {',
+        'var Hello = React.createClass({',
+        '  render: function() {',
         '    return <div>{"Unbalanced braces"}}</div>;',
         '  }',
-        '}'
+        '});'
       ].join('\n'),
-      parser: 'babel-eslint',
+      parserOptions: parserOptions,
       errors: [{message: 'HTML entities must be escaped.'}]
     }
   ]
